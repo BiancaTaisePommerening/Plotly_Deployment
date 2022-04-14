0197a83a9ddad9c363fb66b2c1d4982e75a2c6f7
Bianca <biancataisep@gmail.com>
2022-04-14 15:37:36 -0500

    adding step 3 and 4 from deliverable 1.

--- charts.js
+++ charts.js
@@ -58,5 +58,15 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
 
-  });
+
+  // 3. Create a variable that holds the samples array. 
+  var samplesArray = data.samples; 
+  console.log(samplesArray);
+
+  // 4. Create a variable that filters the samples for the object with the desired sample number.
+  var resultArray = samplesArray.filter(sampleObj => sampleObj.id == sample);
+  console.log(resultArray);
+
+
+});
 }
